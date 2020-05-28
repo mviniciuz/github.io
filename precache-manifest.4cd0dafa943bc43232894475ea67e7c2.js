1e183f6c7e864e34dd1445e49d1a3762835a2485
Marcus Vinicius <m.viniciuz@hotmail.com>
2020-05-28 00:23:04 -0300

    public

---
+++ precache-manifest.4cd0dafa943bc43232894475ea67e7c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59d61871dfd8733475cc8607dddaeeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d8f19f4f19e11689827",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "2d8f19f4f19e11689827",
+    "url": "/static/js/2.9c419132.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9c419132.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c675193b9135c3af20",
+    "url": "/static/js/main.9bdc6746.chunk.js"
+  },
+  {
+    "revision": "d87e8c62f8ae940622d7",
+    "url": "/static/js/runtime-main.a338d56f.js"
+  },
+  {
+    "revision": "8fb5d128ccea41328632005480887f3e",
+    "url": "/static/media/fundacao.8fb5d128.svg"
+  },
+  {
+    "revision": "9bbe0f6499e3ac457275e488333a891d",
+    "url": "/static/media/logoini.9bbe0f64.svg"
+  },
+  {
+    "revision": "4a2cc6922a6e8d23ae37183f839c96c8",
+    "url": "/static/media/video-graca.4a2cc692.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91c5772344f34a6b98b1f3bd94af6519.js"
+  "/precache-manifest.4cd0dafa943bc43232894475ea67e7c2.js"
 );
 
 self.addEventListener('message', (event) => {
